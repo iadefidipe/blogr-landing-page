3401cab40c2c8e6316511cddf638e2d8bbabce29
Israel Adefidipe <israeladefidipe@gmail.com>
2021-06-11 23:37:05 +0100

    Desktop nav functionality ===== 50%

--- app/js/script.js
+++ app/js/script.js
@@ -11,35 +11,51 @@ const headerLinkContainer = document.querySelectorAll(".header__links-container"
 // const headerLinkImg = headerLink.querySelector('img')
 
 
+    btnHamburger.addEventListener("click", function () {
 
-btnHamburger.addEventListener('click', function(){
-
-    if (header.classList.contains('open')){
-        header.classList.remove('open');        
-    } else{
-        header.classList.add('open');
-        headerLink.forEach(element => {
-            element.querySelector('img').src = './images/icon-arrow-dark.svg';
+      if (header.classList.contains("open")) {
+        header.classList.remove("open");
+      } else {
+        header.classList.add("open");
+        headerLink.forEach((element) => {
+          element.querySelector("img").src = "./images/icon-arrow-dark.svg";
         }); //* changes the dropdown icon color in the dropdown
 
-        headerLinks.addEventListener('click', function(e){
-            e.preventDefault();
-            if (e.target.closest('.header__link')){
-                const clicked = e.target.closest(".header__link");
-                console.log(clicked);
+        headerLinks.addEventListener("click", function (e) {
+          e.preventDefault();
+          if (e.target.closest(".header__link")) {
+            const clicked = e.target.closest(".header__link");
 
-                clicked.querySelector('img').classList.toggle('icon-rotate')
+            clicked.querySelector("img").classList.toggle("icon-rotate");
 
-                clicked.nextElementSibling.classList.toggle(
-                    "header__links-sub--active"
-                );
-                
-                
-            }
-        })
-
-    }
-    
+            clicked.nextElementSibling.classList.toggle(
+              "header__links-sub--active"
+            );
+          }
+        });
+      }
+      
+    });
 
 
+// TODO: Implementing menu fade animation
+
+const nav = document.querySelector('nav');
+const logo = document.querySelector('.header__logo');
+
+header.addEventListener('mouseover', function(e){
+  console.log(e.target);
+  if (e.target.classList.contains('nav-fade')){
+    const link = e.target;
+    link.forEach(el => {
+
+    })
+  }
 })
+
+
+
+
+
+
+
