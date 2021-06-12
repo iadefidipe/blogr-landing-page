24caf6c413fcea8588f24014581a8fc7cbceb7c8
Israel Adefidipe <israeladefidipe@gmail.com>
2021-06-12 19:26:53 +0100

    Project completed ===== open to reviewing

--- app/js/script.js
+++ app/js/script.js
@@ -6,12 +6,18 @@ const btnHamburger = document.getElementById('btnHamburger')
 const header = document.querySelector('.header')
 const headerLink = document.querySelectorAll ('.header__link')
 const headerLinks = document.querySelector(".header__links");
-const headerLinkSub = document.querySelector(".header__links-sub");
+const headerLinkSub = document.querySelectorAll(".header__link-sub");
 const headerLinkContainer = document.querySelectorAll(".header__links-container");
+const tablet = window.matchMedia("(max-width: 1024px)");
+const tabletMin = window.matchMedia("(min-width: 1024px)");
+
 // const headerLinkImg = headerLink.querySelector('img')
 
 
-    btnHamburger.addEventListener("click", function () {
+    btnHamburger.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      if(!tablet.matches) return;
 
       if (header.classList.contains("open")) {
         header.classList.remove("open");
@@ -28,30 +34,83 @@ const headerLinkContainer = document.querySelectorAll(".header__links-container"
 
             clicked.querySelector("img").classList.toggle("icon-rotate");
 
-            clicked.nextElementSibling.classList.toggle(
-              "header__links-sub--active"
-            );
+            headerLinkSub.forEach(el => {
+              el.classList.toggle("header__links-sub--active");
+            });
+            
           }
         });
       }
-      
+
     });
+
+//  ***************************************************************************
 
 
 // TODO: Implementing menu fade animation
+const navHover = function(e,opacity){
+
+  if (!tabletMin.matches) return;
+
+  if (e.target.classList.contains('nav-fade')){
+    const link = e.target;
+    const sibling = document.querySelectorAll('.nav-fade');
+
+    sibling.forEach(el => {
+      if(el !== link) el.style.opacity = opacity;
+    })
+
+    logo.style.opacity = opacity;
+  }
+}
 
 const nav = document.querySelector('nav');
 const logo = document.querySelector('.header__logo');
+const navLink = document.querySelectorAll('.nav__link');
 
-header.addEventListener('mouseover', function(e){
-  console.log(e.target);
-  if (e.target.classList.contains('nav-fade')){
-    const link = e.target;
-    link.forEach(el => {
-
-    })
-  }
+header.addEventListener('mouseover',function(e){
+  navHover(e, 0.5);;
 })
+
+header.addEventListener('mouseout', function(e){
+  navHover(e, 1);
+})
+
+// ************************************************************************
+
+navLink.forEach( el => {
+
+  const sibling = el.nextElementSibling;
+  const parent = el.parentElement;
+
+  el.addEventListener("click", function (e) {
+    e.preventDefault();
+    
+
+    if (!tabletMin.matches) return;
+
+    el.querySelector("img").classList.toggle("icon-rotate");
+
+    sibling.classList.toggle("header__link-sub-hide");
+  
+
+    
+  });
+
+  // el.addEventListener("mouseout", function (e) {
+  //   sibling.classList.add("header__link-sub-hide");
+  // });
+
+  // parent.addEventListener("mouseout", function (e) {
+  //   sibling.classList.add("header__link-sub-hide");
+  // });
+  
+
+  
+
+})
+
+
 
 
 
