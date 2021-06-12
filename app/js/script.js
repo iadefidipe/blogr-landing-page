2dcd7cd6da996e621a40235a27ec1258a196e96d
Israel Adefidipe <israeladefidipe@gmail.com>
2021-06-12 20:05:06 +0100

    fixed mobile dropdown JS bug

--- app/js/script.js
+++ app/js/script.js
@@ -10,6 +10,9 @@ const headerLinkSub = document.querySelectorAll(".header__link-sub");
 const headerLinkContainer = document.querySelectorAll(".header__links-container");
 const tablet = window.matchMedia("(max-width: 1024px)");
 const tabletMin = window.matchMedia("(min-width: 1024px)");
+const nav = document.querySelector("nav");
+const logo = document.querySelector(".header__logo");
+const navLink = document.querySelectorAll(".nav__link");
 
 // const headerLinkImg = headerLink.querySelector('img')
 
@@ -27,22 +30,19 @@ const tabletMin = window.matchMedia("(min-width: 1024px)");
           element.querySelector("img").src = "./images/icon-arrow-dark.svg";
         }); //* changes the dropdown icon color in the dropdown
 
-        headerLinks.addEventListener("click", function (e) {
-          e.preventDefault();
-          if (e.target.closest(".header__link")) {
-            const clicked = e.target.closest(".header__link");
+        navLink.forEach( link => {
+          link.addEventListener("click", function (e) {
+            e.preventDefault();
 
-            clicked.querySelector("img").classList.toggle("icon-rotate");
+            link.querySelector("img").classList.toggle("icon-rotate");
 
-            headerLinkSub.forEach(el => {
-              el.classList.toggle("header__links-sub--active");
-            });
-            
-          }
+            link.nextElementSibling.classList.toggle('header__links-sub--active');
         });
-      }
-
-    });
+        
+        
+      });
+    }
+  });
 
 //  ***************************************************************************
 
@@ -64,9 +64,7 @@ const navHover = function(e,opacity){
   }
 }
 
-const nav = document.querySelector('nav');
-const logo = document.querySelector('.header__logo');
-const navLink = document.querySelectorAll('.nav__link');
+
 
 header.addEventListener('mouseover',function(e){
   navHover(e, 0.5);;
@@ -88,6 +86,8 @@ navLink.forEach( el => {
     
 
     if (!tabletMin.matches) return;
+
+    // console.log(e.target);
 
     el.querySelector("img").classList.toggle("icon-rotate");
 
