6e108c4234aa5a4b7a3428d793c8976aacfd33b5
Israel Adefidipe <israeladefidipe@gmail.com>
2021-06-12 22:23:00 +0100

    added observer function

--- app/js/script.js
+++ app/js/script.js
@@ -111,7 +111,25 @@ navLink.forEach( el => {
 })
 
 
+// ********************************************************************************************
 
+// TODO: Revealing elements on scroll
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function(entries, observer){
+  const [entry] = entries;
+
+  if(!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target)
+}
+
+ const sectionObserver = new IntersectionObserver(revealSection, {root:null, threshold: 0.2});
+
+  allSections.forEach( function(section) {
+   sectionObserver.observe(section);
+   section.classList.add('section--hidden');
+  })
 
 
 
